cfffa5cb53296dac5a9e845a9c12ce53a2ed6fd1
Alexander Karpukhin <thealexdev23@gmail.com>
2024-02-02 14:35:53 +0000

    Remake core algorithm Name and precompute more constants

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+MASS = 60
+GRAVITY = 9.8
+
 # Precomputed values for optimization
-WEIGHT = 60 * 9.8
+WEIGHT = MASS * GRAVITY
+VEL_PRECOMP = 2 / MASS
 RES_AIR_PRECOMP = 0.5 * 1.225 * 0.8 * 1.7
 
-TIME_STEP = 0.04
+
+TIME_STEP = 0.01
 
 
 def get_kinetic(x):
     return 2058 - ((0.875 * ((x - 2) ** 2)) * WEIGHT)
-
 
 # Need to start slightly forward as the kinetic energy at displacement 0 is 0 and division by 0 cannot be computed
 dis_y = 0.1
@@ -19,36 +23,51 @@ dis_y_arr = []
 time = 0
 vel = 0
 
+next_kinetic = get_kinetic(dis_y)
+
+iterations = 0
+
 while dis_y <= 2:
-    kinetic = get_kinetic(dis_y)
-    acc = kinetic / 60
-    vel_tmp = vel
-    vel_tmp += acc * TIME_STEP
-    force = kinetic / vel_tmp + WEIGHT
+    prev_dis = dis_y
+
+    vel_new = np.sqrt(VEL_PRECOMP * next_kinetic)
+    acc = (vel_new - vel)/TIME_STEP
+    force = acc * MASS
 
     # Air resistance = 1/2 * air_density * drag_coeff * v² * surface area
     # RES_AIR_PRECOMP has average values for humans and earth
-    res_air = RES_AIR_PRECOMP * (vel_tmp**2)
+    res_air = RES_AIR_PRECOMP * (vel_new**2)
     # As the skate rolls the ground resistance is low
     res_ground = 0.2 * WEIGHT
+    
+    resistance_force = 0
+    resistance_force -= res_air
+    resistance_force -= res_ground
 
-    force -= res_air
-    force -= res_ground
+    force -= resistance_force
 
-    acc = force / 60
+    acc = force / MASS
     vel += acc * TIME_STEP
-
     dis_y += vel * TIME_STEP
 
+    print(f"At t:{time} d:{dis_y} k:{next_kinetic} a:{acc} v:{vel} f:{force}")
+
+    resistance_work = resistance_force * (dis_y - prev_dis)
+    next_kinetic = get_kinetic(dis_y) - resistance_work
+
     dis_y_arr.append(dis_y)
-
-    print(f"At t:{time} d:{dis_y} k:{kinetic} a:{acc} v:{vel} f:{force}")
-
     time += TIME_STEP
+    iterations += 1
 
 
-dis_x_arr = np.arange(0, time, TIME_STEP)
+# dis_x_arr = np.arange(0, time, TIME_STEP)
+prev_x = 0
+dis_x_arr = [prev_x]
+
+for i in range(iterations - 1):
+    prev_x += TIME_STEP
+    dis_x_arr.append(prev_x)
 
 plt.plot(dis_x_arr, dis_y_arr)
 # plt.show()
-plt.savefig("save.png")
+plt.savefig("save2.png")
